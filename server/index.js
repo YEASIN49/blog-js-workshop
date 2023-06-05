e8335ca90bd8ea60f7354b7e188f7f239847a764
Yeasin <myeasin49@gmail.com>
2023-06-05 16:04:16 +0600

    UPDATE: removed commenting

--- server/index.js
+++ server/index.js
@@ -2,12 +2,8 @@ const express = require("express");
 const mongoose = require("mongoose");
 const dotEnv = require("dotenv");
 const cors = require('cors') 
-// const authRoute = require("./routes/auth");
 const userRoute = require("./routes/users");
 const postRoute = require("./routes/posts");
-// const categoryRoute = require("./routes/categories");
-// const multer = require("multer");
-// const path = require("path");
 
 
 //initializing app
@@ -24,16 +20,22 @@ dotEnv.config();
 
 //Database connectivity by mongoose
 mongoose.set('strictQuery', false) // will be true by default from mongoose 7
-mongoose.connect(process.env.MONGO_URI) //will return promise
-	.then(() => console.log("Connected to DB successfully"))
-	.catch(err => console.log(err));
+// mongoose.connect(process.env.MONGO_URI) //will return promise
+// 	.then(() => console.log("Connected to DB successfully"))
+// 	.catch(err => console.log(err));
+
+// connecting to mongodb
+mongoose.connect('mongodb://127.0.0.1:27017/blog-workshop', {
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
+})
+.then(() => console.log("Connected to DB successfully"))
+.catch(err => console.log(err));
 
 
-// app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
 app.use("/api/posts", postRoute);
-// app.use("/api/categories", categoryRoute);
-// app.use("/images",express.static(path.join(__dirname,"/images")));
+
 
 app.listen("5000", () => {
 	console.log("Starting server at http://localhost:5000")
